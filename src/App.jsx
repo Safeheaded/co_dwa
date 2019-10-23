8bdc10d184cea40efbb59e46f301c1d61d08df5d
safeheaded <m.patryk@outlook.com>
2019-10-23 15:52:01 +0200

    Minor error fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import styles from './App.module.sass';
 import Menu from './components/Menu/Menu';
 import SimpleEmissionDisplay from './components/SimpleEmissionDisplay/SimpleEmissionDisplay';
-import EmissionDisplay from './components/EmissionDisplay/EmissionDisplay';
-import AboutSite
+import AboutSite from './components/AboutSite/AboutSite';
 import { Container } from '@material-ui/core';
 
 class App extends Component {
