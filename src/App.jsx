d77b31ad20c5d5e2fe8461e23fa183c61c0d601e
Cpprian <46838495+Cpprian@users.noreply.github.com>
2019-10-25 21:33:11 +0200

    AboutSite in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './App.module.sass';
 import Menu from './components/Menu/Menu';
 import SimpleEmissionDisplay from './components/SimpleEmissionDisplay/SimpleEmissionDisplay';
-//import AboutSite from './components/AboutSite/AboutSite';
+import AboutSite from './components/AboutSite/AboutSite';
 import { Container } from '@material-ui/core';
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
         return (
             <>
                 <Menu />
-                {/* <AboutSite /> */}
+                <AboutSite />
                 <Container maxWidth="xl" className={styles.Container}>
                     <SimpleEmissionDisplay />
                 </Container>
