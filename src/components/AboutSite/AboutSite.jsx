efc73bfb530a92ddda890c1c7ff50d0a4658bf9d
Cpprian <46838495+Cpprian@users.noreply.github.com>
2019-10-25 21:37:01 +0200

    Small changes in AboutSite

--- src/components/AboutSite/AboutSite.jsx
+++ src/components/AboutSite/AboutSite.jsx
@@ -25,8 +25,6 @@ export default function AboutSite() {
     } else {
         localStorage.setItem('root', 'o co chodzi');
 
-        // const logo = require('./projekt-logo-co2.png');
-
         return (
             <div>
                 <Dialog
@@ -48,10 +46,7 @@ export default function AboutSite() {
                     </div>
 
                     <DialogContent>
-                        <DialogContentText
-                            id="alert-dialog-slide-description"
-                            className={styles.DialogContentText}
-                        >
+                        <DialogContentText id="alert-dialog-slide-description">
                             Globalnym problemem, który powstał w połowie XX
                             wieku, a w ostatnich latach staje się jednym z
                             kluczowych jest globalne ocieplenie, czyli
@@ -87,68 +82,3 @@ export default function AboutSite() {
         );
     }
 }
-
-// import React, { Component } from 'react';
-// import styles from './AboutSite.module.sass';
-// import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-
-// class AboutSite extends Component {
-//     state = { showing: true };
-
-//     render() {
-//         if (localStorage.getItem('root')) {
-//             return null;
-//         } else {
-//             localStorage.setItem('root', 'o co chodzi');
-
-//             const { showing } = this.state;
-//             const logo = require('./projekt-logo-co2.png');
-
-//             return !showing ? null : (
-//                 <div className={styles.popup}>
-//                     <div className={styles.popup__content}>
-//                         <div className={styles.popup__header}>
-//                             <h1>O co chodzi?</h1>
-//                             <img
-//                                 src={logo}
-//                                 className={styles.popup__logo}
-//                                 alt="logo"
-//                             />
-//                         </div>
-//                         <div className={styles.popup__text}>
-//                             <h2>Witamy w aplikacji co2!</h2>
-//                             <p>
-//                                 Celem aplikacji, który nam przyświęcał jest
-//                                 podniesienie świadomości ludzi odnośnie tego
-//                                 jakie środki transportu odpowiednio wytwarzają
-//                                 ilość dwutlenku węgla do naszego środowiska.
-//                             </p>
-
-//                             <p>
-//                                 Aplikacja w prosty sposób umożliwia
-//                                 użytkownikowi sprawdzenia tego wybierając i
-//                                 wpsiując określone wartości.
-//                             </p>
-//                             <br></br>
-//                             <p>
-//                                 Zapraszamy do skorzystania z aplikacji. Zespół
-//                                 Open Learn Giganty Mocy
-//                             </p>
-//                             <div
-//                                 className={styles.popup__btn}
-//                                 onClick={() =>
-//                                     this.setState({ showing: !showing })
-//                                 }
-//                             >
-//                                 <ThumbUpIcon></ThumbUpIcon>
-//                                 &nbsp;Rozumiem
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             );
-//         }
-//     }
-// }
-
-// export default AboutSite;
